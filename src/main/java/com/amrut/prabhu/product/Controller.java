824cc4afee2fc4f472d5deb3a4a4532845203a77
nguyenthaidien <nguyenthaidien@gmail.com>
2025-07-11 14:11:24 +0700

    ssss

--- src/main/java/com/amrut/prabhu/product/Controller.java
+++ src/main/java/com/amrut/prabhu/product/Controller.java
@@ -3,36 +3,32 @@ package com.amrut.prabhu.product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import javax.annotation.security.RolesAllowed;
 import java.security.Principal;
 
+@RefreshScope
 @RestController
 public class Controller {
 
-    //@GetMapping("/product")
+
+    @Value("${welcome.message}")
+    private String message;
+
+    @GetMapping("/welcome")
+    public String getMessage() {
+        return message;
+    }
+
+
     
-    @GetMapping("/product/{id}")
+    @GetMapping("/user/{id}")
     //@RolesAllowed({"product_read"})
     public String getUser(@PathVariable("id") String userId) {
         return "User ID: " + userId;
     }
 
-/*     public String greet() {
-        return "Hello, welcome to our API!";
-    } */
-/*     public String getProduct(Principal principal) {
-        return "Response from Product Service, User Id:" + principal.getName();
-    } */
-/*    public String getUser(@PathVariable("id") String userId) {
-        return "User ID: " + userId;
-    } */
-/*     @GetMapping("/products/{category}/{id}")
-    public String getProduct(
-        @PathVariable("category") String category, 
-        @PathVariable("id") String productId
-    ) {
-        return "Category: " + category + ", Product ID: " + productId;
-    } */
+
 
 }
