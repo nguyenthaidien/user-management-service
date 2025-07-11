8a969157b10dfb42f556a884acc6ed88da3985ed
nguyenthaidien <nguyenthaidien@gmail.com>
2025-07-11 14:11:24 +0700

    ssss

--- src/main/java/com/amrut/prabhu/product/Controller.java
+++ src/main/java/com/amrut/prabhu/product/Controller.java
@@ -3,12 +3,24 @@ package com.amrut.prabhu.product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import javax.annotation.security.RolesAllowed;
 import java.security.Principal;
 
+@RefreshScope
 @RestController
 public class Controller {
+
+
+    @Value("${welcome.message}")
+    private String message;
+
+    @GetMapping("/welcome")
+    public String getMessage() {
+        return message;
+    }
+
 
     //@GetMapping("/product")
     
